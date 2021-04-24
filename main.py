c1831d126f1a8e3af548583f3659acf07fa4157a
Luiz Augusto Frazatto Fernandes <luiz.a.frazatto.fernandes@gmail.com>
2021-04-24 13:55:54 +0000

    main: using name with print_my_name

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("yet another git course")
-print("pointless change to justify another command")
+import name
+
+print("Yet another git course")
+print("Pointless change to justify another command.")
+
+name.print_my_name()
