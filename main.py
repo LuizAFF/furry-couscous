95a3c439ea06089aac951447189e7d5cdedfa2e7
Luiz Augusto Frazatto Fernandes <luiz.a.frazatto.fernandes@gmail.com>
2021-04-24 14:36:11 +0000

    main: Add a comment and a stupid print.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Adding this comment to prove a point
 import name
 
 print("Yet another git course!")
@@ -6,3 +7,4 @@ print("Pointless change to justify another command.")
 name.print_my_name()
 
 print("Final print!")
+print(3.14159265)
